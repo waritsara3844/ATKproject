a75022aff28e05d14aff210e12f89b3272ab3e94
waritsara3844 <70258816+waritsara3844@users.noreply.github.com>
2023-03-14 18:30:16 +0700

    first

---
+++ src/pages/FormPage.vue
@@ -0,0 +1,104 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-form @submit="onSubmit" @reset="onReset">
+        <div class="row q-ma-md">
+          <div class="col-3 q-mr-md">
+            Firstname
+            <q-input
+              filled
+              v-model="name"
+              label="Your name *"
+              hint="Name and surname"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+          </div>
+          <div class="col-4 q-mr-md">
+            Lastname
+            <q-input
+              filled
+              type="number"
+              v-model="age"
+              label="Your age *"
+              lazy-rules
+              :rules="[
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age',
+              ]"
+            />
+          </div>
+          <div class="col-4 q-mr-md">
+            Lastname
+            <q-input
+              filled
+              type="number"
+              v-model="age"
+              label="Your age *"
+              lazy-rules
+              :rules="[
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age',
+              ]"
+            />
+          </div>
+        </div>
+        <div class="row q-ma-md">
+          <div class="col-3 q-mr-md">
+            Firstname
+            <q-input
+              filled
+              v-model="name"
+              label="Your name *"
+              hint="Name and surname"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+          </div>
+          <div class="col-4 q-mr-md">
+            Lastname
+            <q-input
+              filled
+              type="number"
+              v-model="age"
+              label="Your age *"
+              lazy-rules
+              :rules="[
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age',
+              ]"
+            />
+          </div>
+          <div class="col-4 q-mr-md">
+            Lastname
+            <q-input
+              filled
+              type="number"
+              v-model="age"
+              label="Your age *"
+              lazy-rules
+              :rules="[
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age',
+              ]"
+            />
+          </div>
+        </div>
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormPage",
+});
+</script>
+
+<style></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <q-page padding align="center">
+        <img
+          src="https://dorm.psu.ac.th/system/files/images/psu-logo.png"
+          style="height: 140px"
+          class="q-mt-xl"
+        />
+        <div class="row q-mt-lg text-h5 text-bold">
+          <div class="col">Welcome to PSU ATK Form</div>
+        </div>
+        <div class="row q-mt-lg text-left text-bold">
+          <div class="col">Username</div>
+        </div>
+        <div class="row q-mt-md">
+          <div class="col">
+            <q-input outlined v-model="text" label="Enter your Username" />
+          </div>
+        </div>
+        <div class="row q-mt-lg text-left text-bold">
+          <div class="col">Password</div>
+        </div>
+        <div class="row q-mt-md">
+          <div class="col">
+            <q-input outlined v-model="text" label="Enter your Password" />
+          </div>
+        </div>
+        <div class="row q-mt-md">
+          <div class="col">
+            <q-btn
+              unelevated
+              rounded
+              color="primary"
+              label="Sign in"
+              no-caps
+              style="width: 90%"
+            />
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  // data: {
+  //   return: {
+  //     email: "",
+  //     password: "",
+  //   },
+  // },
+  // method: {
+  //   // chekLogin() {
+  //   //   (this.email = email), (this.password = password);
+  //   // },
+  // },
+});
+</script>
