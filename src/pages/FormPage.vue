f33656b3f2f7b7aece0ff7fad56f60bae31fb343
waritsara3844 <70258816+waritsara3844@users.noreply.github.com>
2023-04-17 18:18:48 +0700

    change q-upload

--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <q-page class="q-ma-md flex flex-center">
+      <q-page class="flex flex-center q-mx-md" padding>
         <div class="row-12">
           <div class="col">
             <q-card>
@@ -20,14 +20,25 @@
               <div class="row text-bold text-h6">ATK Report</div>
               <q-input v-model="username" label="Username" />
               <q-input v-model="studentId" label="Student ID" />
-              <q-uploader
-                :value="selectedImage"
-                :max-file-size="maxFileSize"
-                :extensions="allowedExtensions"
-                @input="onImageInput"
-                label="Upload Image"
-                class="q-mt-md"
+              <q-option-group
+                v-model="atkreuslt"
+                color="secondary"
+                :options="options"
+                inline
               />
+              <q-file
+                standout
+                v-model="upload_atk"
+                label="Your Avatar"
+                max-file-size="1048576"
+                accept=".jpg, .jpeg, .png"
+                @rejected="onRejected"
+                @update:model-value="uploadFile()"
+              >
+                <template #append>
+                  <q-icon name="attach_file" />
+                </template>
+              </q-file>
               <q-btn
                 type="submit"
                 label="Submit"
@@ -43,32 +54,56 @@
 </template>
 
 <script>
-import { QForm, QInput, QUploader, QBtn } from "quasar";
+import { QForm, QInput, QBtn } from "quasar";
 import { defineComponent } from "vue";
+import { ref } from "vue";
 
 export default defineComponent({
   name: "FormPage",
   components: {
     QForm,
     QInput,
-    QUploader,
     QBtn,
+  },
+  setup() {
+    return {
+      atkreuslt: ref("negative"),
+      options: [
+        {
+          label: "Negative(-)",
+          value: "negative",
+        },
+        {
+          label: "Positive(+)",
+          value: "positive",
+        },
+      ],
+    };
   },
   data() {
     return {
       username: "",
       studentId: "",
-      selectedImage: null,
-      maxFileSize: 5000000, // 5 MB
-      allowedExtensions: ["jpg", "jpeg", "png"],
+      imageUrl: "",
+      upload_atk: null,
     };
   },
   methods: {
-    submitForm() {
-      // submit form logic
+    submitForm() {},
+
+    uploadFile() {
+      this.imageUrl = URL.createObjectURL(this.upload_atk);
     },
-    onImageInput(selectedFiles) {
-      this.selectedImage = selectedFiles[0];
+    onRejected(rejectedEntries) {
+      let msg;
+      if (rejectedEntries[0].failedPropValidation == "accept")
+        msg = "Only images (jpg, jpeg, png) are allowed.";
+      else if (rejectedEntries[0].failedPropValidation == "max-file-size")
+        msg = "File size cannot be larger than 1MB.";
+      Notify.create({
+        type: "negative",
+        message: msg,
+      });
     },
   },
 });
